139d56344c3ebbc7aa208d11d9f227c35b1d1a7c
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-02-15 16:49:42 +0100

    refactor(GUIS/WEB/ANGULAR): :art: add context

--- _tools/guis/web/angular/setup-project.js
+++ _tools/guis/web/angular/setup-project.js
@@ -11,11 +11,11 @@ const GUIS_BASE_PATH = TOOLS_PATH.replace("/_tools", "");
 
 // Check if the project directory already exists
 if (!fs.existsSync(GUIS_BASE_PATH)) {
-    console.error(
-        `Error: The GUIS_BASE_PATH does not exists at ${GUIS_BASE_PATH}`
-    );
-    console.log("Creating the directory now...");
-    execSync(`mkdir -p ${GUIS_BASE_PATH}`);
+  console.error(
+    `Error: The GUIS_BASE_PATH does not exists at ${GUIS_BASE_PATH}`
+  );
+  console.log("Creating the directory now...");
+  execSync(`mkdir -p ${GUIS_BASE_PATH}`);
 }
 
 // Log the GUIS_BASE_PATH to verify it's correct
@@ -23,9 +23,9 @@ console.log("GUIS_BASE_PATH: ", GUIS_BASE_PATH);
 
 // Validate input arguments
 if (process.argv.length < 3) {
-    console.error("Error: Please provide a project name as an argument");
-    console.error("Usage: bun run setup:guis:web:angular <project_name>");
-    process.exit(1);
+  console.error("Error: Please provide a project name as an argument");
+  console.error("Usage: bun run setup:guis:web:angular <project_name>");
+  process.exit(1);
 }
 
 // Install Angular CLI globally
@@ -39,16 +39,16 @@ console.log("PROJECT_PATH: ", PROJECT_PATH);
 
 // Format the project name for display
 const FORMATTED_PROJECT_NAME = PROJECT_NAME.replace(/_/g, " ").replace(
-    /\b\w/g,
-    (char) => char.toUpperCase()
+  /\b\w/g,
+  (char) => char.toUpperCase()
 );
 
 // Check if the project directory already exists
 if (fs.existsSync(PROJECT_PATH)) {
-    console.error(
-        `Error: The project "${PROJECT_NAME}" already exists at ${GUIS_BASE_PATH}`
-    );
-    process.exit(1);
+  console.error(
+    `Error: The project "${PROJECT_NAME}" already exists at ${GUIS_BASE_PATH}`
+  );
+  process.exit(1);
 }
 
 // Change directory to the project directory
@@ -57,7 +57,9 @@ process.chdir(GUIS_BASE_PATH);
 
 // Create the Angular project
 console.log(`Creating Angular project at ${PROJECT_PATH}`);
-execSync(`ng new ${PROJECT_NAME} --style=css --ssr=false --skip-install`, { stdio: "inherit" });
+execSync(`ng new ${PROJECT_NAME} --style=css --ssr=false --skip-install`, {
+  stdio: "inherit",
+});
 
 // Change directory to the project directory
 console.log(`Changing directory to PROJECT_PATH: ${PROJECT_PATH}`);
@@ -71,11 +73,12 @@ execSync(`bun install`, { stdio: "inherit" });
 // Initialize Tailwind CSS
 console.log(`Installing Tailwind CSS for "${PROJECT_NAME}"`);
 execSync(`bun install tailwindcss @tailwindcss/postcss postcss`, {
-    stdio: "inherit",
+  stdio: "inherit",
 });
 
 // Add Tailwind CSS to the Angular JSON file
-const postcssrcjsonContent = '{  "plugins": {    "@tailwindcss/postcss": {}  }}';
+const postcssrcjsonContent =
+  '{  "plugins": {    "@tailwindcss/postcss": {}  }}';
 fs.writeFileSync(".postcssrc.json", postcssrcjsonContent, "utf-8");
 
 console.log("Configuring Tailwind CSS...");
@@ -83,23 +86,21 @@ const indexCssPath = path.join(PROJECT_PATH, "src", "styles.css");
 // Read the current content of the file
 const currentContent = fs.readFileSync(indexCssPath, "utf-8");
 // Prepend the new content to the existing content
-const newContent = currentContent +
-    '@import "tailwindcss";\n';
+const newContent = currentContent + '@import "tailwindcss";\n';
 // Write the new content back to the file
 fs.writeFileSync(indexCssPath, newContent, "utf-8");
 
 console.log("Updating index.html...");
 const indexHtmlPath = path.join(PROJECT_PATH, "src/index.html");
 const indexHtmlContent = fs
-    .readFileSync(indexHtmlPath, "utf-8")
-    .replace(/<title>.*<\/title>/, `<title>${FORMATTED_PROJECT_NAME}</title>`);
+  .readFileSync(indexHtmlPath, "utf-8")
+  .replace(/<title>.*<\/title>/, `<title>${FORMATTED_PROJECT_NAME}</title>`);
 fs.writeFileSync(indexHtmlPath, indexHtmlContent);
 
 // Updating the App.tsx
 console.log("Updating the app.component.html...");
 const appHtmlPath = path.join(PROJECT_PATH, "src", "app", "app.component.html");
-const updatedAppHtmlContent =
-    `<div class="h-screen w-full flex items-center justify-center"><h1 class="text-3xl font-bold underline">${FORMATTED_PROJECT_NAME}</h1></div>`;
+const updatedAppHtmlContent = `<div class="h-screen w-full flex items-center justify-center"><h1 class="text-3xl font-bold underline">${FORMATTED_PROJECT_NAME} with angular</h1></div>`;
 fs.writeFileSync(appHtmlPath, updatedAppHtmlContent, "utf-8");
 console.log("✅ The app.component.html has been updated");
 
