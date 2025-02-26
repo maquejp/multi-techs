d6ce90f758c448c92e68033b08037e8b845198db
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-02-26 16:28:13 +0100

    refactor(DATABASES/ORACLE): :recycle: Move composer file into project folder

--- _tools/databases/oracleenterprise/start-oracleenterprise.js
+++ _tools/databases/oracleenterprise/start-oracleenterprise.js
@@ -1,48 +1,58 @@
-import { execSync, spawn } from "child_process";
-import { existsSync, mkdirSync, writeFileSync } from "fs";
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
 
-// Get script directory
-const __filename = fileURLToPath(import.meta.url);
-const SCRIPT_DIR = dirname(__filename);
-console.log(`Script directory: ${SCRIPT_DIR}`);
+// Get the absolute path to the _tools folder
+const TOOLS_PATH = path.dirname(new URL(import.meta.url).pathname);
+console.log("TOOLS_PATH: ", TOOLS_PATH);
 
-// Create required directories
-const dataDir = resolve("./databases/oracleenterprise/data");
-const readmeFile = resolve("./databases/oracleenterprise/README.md");
+// Remove "/_tools" to get the correct base path for the Oracle Enterprise project
+const DATABASES_BASE_PATH = path.join(TOOLS_PATH.replace("/_tools", ""));
+console.log("DATABASES_BASE_PATH: ", DATABASES_BASE_PATH);
+const DATABASES_DATA_PATH = path.join(DATABASES_BASE_PATH, "data");
+console.log("DATABASES_DATA_PATH: ", DATABASES_DATA_PATH);
+const README_FILE_PATH = path.join(DATABASES_BASE_PATH, "README.md");
+console.log("README_FILE_PATH: ", README_FILE_PATH);
 
-if (!existsSync(dataDir)) {
-    mkdirSync(dataDir, { recursive: true });
-    console.log("Created directories: ./databases/oracleenterprise/data");
+// Create the project directory
+console.log(`Creating project directory at ${DATABASES_DATA_PATH}...`);
+fs.mkdirSync(DATABASES_DATA_PATH, { recursive: true });
 
-    try {
-        // Set permissions (Linux/macOS only)
-        if (process.platform !== "win32") {
-            execSync(`chmod 777 -R ./databases`);
-            // execSync(`chmod 777 -R "${dataDir}"`);
-        } else {
-            console.log("Skipping chmod for Windows platform.");
-        }
-    } catch (error) {
-        console.warn("Could not set permissions on:", dataDir);
+try {
+    // Set permissions (Linux/macOS only)
+    if (process.platform !== "win32") {
+        execSync(`chmod 777 -R ./databases`);
+        // execSync(`chmod 777 -R "${DATABASES_BASE_PATH}"`);
+    } else {
+        console.log("Skipping chmod for Windows platform.");
     }
+} catch (error) {
+    console.warn("Could not set permissions on:", DATABASES_BASE_PATH);
+}
 
-    const readmeContent = `# oracleenterprise
+// Copy the docker compose file
+console.log("Copying docker-compose.yml...");
+fs.copyFileSync(
+    path.join(TOOLS_PATH, "templates", "docker-compose.yml"),
+    path.join(DATABASES_BASE_PATH, "docker-compose.yml")
+);
+
+const README_CONTENT = `# Oracle Enterprise
 
 Connection details:
   - Host: localhost
-  - Port: 3306
+  - Port: 1521
   - Username: system
-
-Despite the service is running, the first time it will take longer for the oracle database to be ready. Please check docker's log for the container!
 `;
-    writeFileSync(readmeFile, readmeContent);
-    console.log("Created README.md with connection details.");
-}
+fs.writeFileSync(README_FILE_PATH, README_CONTENT);
+console.log("Created README.md with connection details.");
 
-// Start oracleenterprise container
-console.log("Starting oracleenterprise container...");
+// Change directory to the project directory
+console.log(`Changing directory to PROJECT_PATH: ${DATABASES_BASE_PATH}`);
+process.chdir(DATABASES_BASE_PATH);
+
+// Start Oracle Enterprise container
+console.log("Starting Oracle Enterprise container...");
 try {
 
     // Create the multitech-common network if needed
@@ -54,16 +64,16 @@ try {
         console.error("Error creating common network:", error.message);
     }
 
-    execSync(`docker compose -f "${SCRIPT_DIR}/docker-compose.yml" up -d`, {
+    execSync(`docker compose up --build -d`, {
         stdio: "inherit",
     });
 } catch (error) {
-    console.error("Error starting oracleenterprise container:", error.message);
+    console.error("Error starting Oracle Enterprise container:", error.message);
     process.exit(1);
 }
 
-// Wait for the oracleenterprise container to be healthy
-console.log("Waiting for container to be healthy...");
+// Wait for the api platform container to be healthy
+console.log("Waiting for Oracle Enterprise container to be healthy...");
 const checkContainerHealth = () => {
     try {
         const status = execSync(
@@ -79,15 +89,14 @@ const checkContainerHealth = () => {
 
 const waitForHealthy = async () => {
     while (!checkContainerHealth()) {
-        console.log("Waiting for oracleenterprise to be ready...");
+        console.log("Waiting for Oracle Enterprise to be ready...");
         await new Promise((resolve) => setTimeout(resolve, 5000));
     }
-    console.log("oracleenterprise is now running and healthy!");
+    console.log("Oracle Enterprise is now running and healthy!");
     console.log("Connection details:");
     console.log("  - Host: localhost");
-    console.log("  - Port: 3306");
+    console.log("  - Port: 1521");
     console.log("  - Username: system");
-    console.log("Despite the service is running, the first time it will take longer for the oracle database to be ready. Please check docker's log for the container!");
 };
 
 waitForHealthy();
