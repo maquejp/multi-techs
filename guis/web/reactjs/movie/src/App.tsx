3f42d2536ae2534fbae53fe8b15146bf46500d57
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-02-27 15:18:15 +0100

    feat(GUIS/WEB/REACTJS/MOVIE): :tada: Initial app for the project

---
+++ guis/web/reactjs/movie/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="h-screen w-full flex items-center justify-center"><h1 className="text-3xl font-bold underline">Movie with reactjs (vite)</h1></div>
+  )
+}
+
+export default App
