e02757fb711b8ad78f4eea465c12b930f8ff7a0b
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-03-07 13:42:30 +0100

    refactor(GUIS/WEB/REACTJS/MOVIE): :recycle: Use bearer instead of url query

--- guis/web/reactjs/movie/src/helpers/tmdb_api_helper.ts
+++ guis/web/reactjs/movie/src/helpers/tmdb_api_helper.ts
@@ -6,10 +6,8 @@ import {
 } from "@/interfaces/tmdb_interfaces";
 
 const BASE_URL = appEnv.API_URL;
-const API_KEY = appEnv.API_KEY;
-
-// Define types for our API URIs structure
-type EndpointFunction = () => string;
+// const API_KEY = appEnv.API_KEY;
+const API_READ_ACCESS_KEY = appEnv.API_READ_ACCESS_KEY;
 
 export enum TmdbContext {
   MoviePopular = "movie.popular",
@@ -24,56 +22,40 @@ export enum TmdbContext {
 
 // Define types for our API URIs structure: Movie
 interface MovieEndpoints {
-  popular: EndpointFunction;
-  upcoming: EndpointFunction;
-  topRated: EndpointFunction;
-  nowPlaying: EndpointFunction;
+  popular: string;
+  upcoming: string;
+  topRated: string;
+  nowPlaying: string;
 }
 
 // Define types for our API URIs structure: TV
 interface TvEndpoints {
-  popular: EndpointFunction;
-  trendingWeek: EndpointFunction;
-  topRated: EndpointFunction;
-  airingToday: EndpointFunction;
+  popular: string;
+  trendingWeek: string;
+  topRated: string;
+  airingToday: string;
 }
 
-// Define types for our API URIs structure
 interface TmdbApiUris {
   movie: MovieEndpoints;
   tv: TvEndpoints;
 }
 
-type Category = keyof TmdbApiUris;
-
-const urlSearchParamsApiKey = new URLSearchParams({ api_key: API_KEY });
-
-// Define our API URIs
 const tmdbApiUris: TmdbApiUris = {
   movie: {
-    popular: (): string => `${BASE_URL}/movie/popular?${urlSearchParamsApiKey}`,
-    upcoming: (): string =>
-      `${BASE_URL}/movie/upcoming?${urlSearchParamsApiKey}`,
-    topRated: (): string =>
-      `${BASE_URL}/movie/top_rated?${urlSearchParamsApiKey}`,
-    nowPlaying: (): string =>
-      `${BASE_URL}/movie/now_playing?${urlSearchParamsApiKey}`,
+    popular: `${BASE_URL}/movie/popular`,
+    upcoming: `${BASE_URL}/movie/upcoming`,
+    topRated: `${BASE_URL}/movie/top_rated`,
+    nowPlaying: `${BASE_URL}/movie/now_playing`,
   },
   tv: {
-    popular: (): string => `${BASE_URL}/tv/popular?${urlSearchParamsApiKey}`,
-    trendingWeek: (): string =>
-      `${BASE_URL}/trending/tv/week?${urlSearchParamsApiKey}`,
-    topRated: (): string => `${BASE_URL}/tv/top_rated?${urlSearchParamsApiKey}`,
-    airingToday: (): string =>
-      `${BASE_URL}/tv/airing_today?${urlSearchParamsApiKey}`,
+    popular: `${BASE_URL}/tv/popular`,
+    trendingWeek: `${BASE_URL}/trending/tv/week`,
+    topRated: `${BASE_URL}/tv/top_rated`,
+    airingToday: `${BASE_URL}/tv/airing_today`,
   },
 };
 
-/**
- * Get TMDB API URL using dot notation context
- * @param context - Context in dot notation format, e.g. 'movie.popular'
- * @returns The URL string for the specified API endpoint
- */
 export const tmdbGetApiUrl = (context: string): string => {
   const segments = context.split(".");
   if (segments.length !== 2) {
@@ -83,35 +65,30 @@ export const tmdbGetApiUrl = (context: string): string => {
   }
 
   const [categoryStr, endpointStr] = segments;
-
-  // Type validation for category
-  const category = categoryStr as Category;
-
-  // Ensure the category exists in tmdbApiUris
+  const category = categoryStr as keyof TmdbApiUris;
   const categoryEndpoints = tmdbApiUris[category];
 
-  // Ensure the endpoint exists for the given category
   if (!(endpointStr in categoryEndpoints)) {
     throw new Error(
       `Endpoint "${endpointStr}" not found in category "${categoryStr}"`
     );
   }
 
-  // Access the function directly
-  return categoryEndpoints[endpointStr as keyof typeof categoryEndpoints]();
+  return categoryEndpoints[endpointStr as keyof typeof categoryEndpoints];
 };
 
-/**
- * Get data from the TMDB API
- * @param context - Context in dot notation format, e.g. 'movie.popular'
- * @returns Data from the TMDB API
- */
 export const tmdbFetchData = async (
   context: TmdbContext
 ): Promise<tmdbFetchMovieType> => {
   try {
     const apiUrl = tmdbGetApiUrl(context);
-    const response = await axios.get<tmdbMovieResponseType>(apiUrl);
+    const response = await axios.get<tmdbMovieResponseType>(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${API_READ_ACCESS_KEY}`,
+        Accept: "application/json",
+      },
+    });
+
     if (response.status !== 200) {
       return { message: "Error", status: response.status };
     }
